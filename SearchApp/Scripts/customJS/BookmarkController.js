68dd31f185f3da54b39d1dd1cabd2adb2efae41f
Aliza Koresh <lizzie.kor@gmail.com>
2018-07-31 21:18:16 +0300

    Add project files.

---
+++ SearchApp/Scripts/customJS/BookmarkController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+
+    var bookmarksObj;//= { name: '', avatar: '' };
+    var bmArry = new Array();
+
+    app.controller('bmCtrl', function ($scope, $http) {
+        $scope.countBm = sessionStorage.length; //count of saved bookmarks
+
+        //loop over session, parsing into JSON in order to retrieve the data as a key-value structure
+        for (var i = 0; i < sessionStorage.length; i++) {
+            bookmarksObj = JSON.parse(sessionStorage.getItem(i));
+            bmArry.push(bookmarksObj);// save in a tmp array
+        }
+        // the entire array will be alternate in ng-repeat directive
+        $scope.bms = bmArry;
+
+    });
+
+
+}).call(angular);
---
+++ SearchApp/Scripts/customJS/IndexController.js
@@ -0,0 +1,40 @@
+﻿(function () {
+
+    var txtSearchUrl = "https://api.github.com/search/repositories?q="; //the given url to search for the key
+    var txtSearch = "";
+    var bookmarksObj = { name: '', avatar: '' };
+
+    app.controller('searchCtrl', function ($scope, $http) {
+        //clicking the search button or pressing the Enter button:
+        $scope.onClickSearch = function () {
+            if ($scope.searchKey==undefined) {
+                alert("Please make sure to insert a key to search");
+               return;
+            }
+            txtSearch = txtSearchUrl + $scope.searchKey; //set the entire url to search by
+            $scope.searchTitle = "Search Results: ";
+            $scope.keyInUrl = "search's been made for url: " + txtSearch; //user indication
+
+            //get the API data 
+            $http.get(txtSearch).then(function (response) {
+                $scope.countRes = "found " + response.data.items.length + " items";
+                //alternate the data in ng-repeat directive
+                $scope.searchResults = response.data.items;
+            });
+
+            //save bookmark into session by using tmp var bookmarksObj in a key-value structure
+            //containing the keys as asked: name, owner avatar
+            $scope.saveBookmark = function (fullRepositoryName, avatar) {    
+                bookmarksObj.name = fullRepositoryName;
+                bookmarksObj.avatar = avatar;
+
+                //item save into session, while the key of the session is increased by 1 (starting from 0)
+                sessionStorage.setItem(sessionStorage.length, JSON.stringify(bookmarksObj));              
+
+                $scope.countBms = ", "+sessionStorage.length + " bookmarks saved";
+            }
+        };
+    });
+
+
+}).call(angular);
